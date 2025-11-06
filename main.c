088c5748223c2448f0d9b11a335202f52dddd52b
Logan Lucas <logan.lucas83@gmail.com>
2025-11-06 12:40:06 +0100

    blinky

--- main.c
+++ main.c
@@ -1,14 +1,23 @@
 #include <stdint.h>
 
+// Not true time
+void delay(uint32_t time) {
+    for (uint32_t i = 0; i < time; i++)
+        for (uint32_t j = 0; j < time; j++) {}
+}
+
 int main(void) {
     *((volatile uint32_t*) 0x4002104C) |= 1 << 1;
 
     *((volatile uint32_t*) 0x48000400) |= 1 << 16;
     *((volatile uint32_t*) 0x48000400) &= ~(1 << 17);
 
-    *((volatile uint32_t*) 0x48000414) |= 1 << 8;
-
     for (;;) {
+        *((volatile uint32_t*) 0x48000414) |= 1 << 8;
+        delay(1000);
+
+        *((volatile uint32_t*) 0x48000414) &= ~(1 << 8);
+        delay(1000);
     }
 
     return 0;
